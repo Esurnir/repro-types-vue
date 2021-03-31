60e6576a25fe512a70f26b58c07a5281b2640e4b
Jean-Baptiste Zeller <jbzeller@eudonet.com>
2021-03-31 16:02:02 +0200

    minimal repro

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <my-component-1 id="1" />
+    <my-component-2 id="2" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue, { AsyncComponent } from 'vue';
+import Loading from './components/Loading.vue';
+
+const MyComponent1: AsyncComponent = () => ({
+  component: () => import('./components/MyComponent.vue'),
+  loading: Loading,
+});
+
+const MyComponent2: AsyncComponent = () => ({
+  // @ts-expect-error Ceci n'est pas une factory qui retourne la fonction mais la promesse réel
+  component: import('./components/MyComponent.vue'),
+  loading: Loading,
+});
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld,
+    MyComponent1,
+    MyComponent2,
   },
 });
 </script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Loaded ! id {{ id }}</p>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: ['id'],
+});
+</script>
